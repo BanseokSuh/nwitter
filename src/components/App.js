b9c1e9bc4278974304dd9a4d02e2bfe974936fc2
banseokSuh <still3028@gmail.com>
2021-05-21 00:45:31 +0900

    nwitter ~attachment

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,23 @@ import {authService} from 'fbase'
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
+        // setIsLoggedIn(true);
+        setUserObj(user);
       }
+      // else {
+      //   setIsLoggedIn(false);
+      // }
       setInit(true);
     })
   }, [])
   return <>
-    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
-    <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "Initializing..."}
   </>;
 }
 
